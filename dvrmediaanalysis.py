bcc8162a5f767cd6ab13ffbd768db457e9a3576c
EcurbNosneb <brucewilliambenson@gmail.com>
2020-12-13 13:51:00 -0600

    Convert encode times

--- dvrmediaanalysis.py
+++ dvrmediaanalysis.py
@@ -1,6 +1,7 @@
 ''' Analyze movies, etc., recorded on DVRs '''
 
 import json
+
 media_file_name = "recordings.json"
 
 def get_media(json_file_name):
@@ -15,8 +16,12 @@ def remove_list (media_dict):
     #Run though raw dict
     for movie in media_dict:
         #Add new dict entry to simple_dict
-        simple_dict.update({movie:(media_dict[movie])[0]})
+        simple_dict.update({movie:media_dict[movie][0]})
     return (simple_dict)
+
+def convert_encode_time(media_dict):
+    ''' Convert all EncodeTime to a valid datetime object '''
+    assert False, "Remove this statement and update this def to convert the media_dict encode_time strings to a datetime objects" 
 
 def main():
     """
@@ -24,11 +29,11 @@ def main():
     """
 
     movie = "2019 Christmas- A First Look Preview Special_HALLHD_2019_07_13_21_00_01.wtv"
-    #Show a  movie before being flattened
-    print (get_media(media_file_name)[movie])
-    #Show the same movie after being flattned by replacing the list
+    #Show a  movie with the original EncodeTime
     print (remove_list(get_media(media_file_name))[movie])
-    
+    #Show the same movie with the updated EncodeTime
+    print (convert_encode_time(remove_list(get_media(media_file_name)))[movie])
+
 
 # This little chunk of code allows this python program to be either used directly or imported into another program
 if __name__ == "__main__":
