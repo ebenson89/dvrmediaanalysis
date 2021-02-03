affca4b1eb598fdba7c4b8c8e1481c26ba1e6fd0
ebenson89 <embox4@gmail.com>
2021-02-02 19:17:28 -0600

    refector code to class format

--- dvrmediaanalysis.py
+++ dvrmediaanalysis.py
@@ -6,117 +6,149 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series
 import matplotlib.pyplot as plt
+import os
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIntValidator
 
+graph_file_name = "graph_skeletons.json"
 media_file_name = "recordings.json"
+qtCreatorFile = "mainwindow.ui" # GUI Design file
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
-def get_media(json_file_name):
-    #Open json file
-    with open(media_file_name) as media_file:
-        #Returns json obj as a dictionary
-        return json.load(media_file)
+class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.graph_options()
 
-def remove_list (media_dict):
-    #Flatten each dictionary entry by replacing the list with the value that was in the list
-    simple_dict = {}
-    #Run though raw dict
-    for movie in media_dict:
-        #Add new dict entry to simple_dict
-        simple_dict.update({movie:media_dict[movie][0]})
-    return (simple_dict)
+        #self.clean_moviedict = cleanup_time(remove_list(get_data(media_file_name)))
+        #self.main_movies_dataframe = dict_to_dataframe(clean_moviedict)
+        #self.build_graph_button.clicked.connect(self.build_graph)
 
-def show_length (time_string):
-    #Round the times in the runtime to the nearest half hour
-    minutes = int(time_string[2:4])
-    hours = int(time_string[:1])
-    
-    #round to .5 if under 30
-    if minutes < 30:
-        rounded_time = time_string[:1] + ".5"
-    #round to .5 if 15 to 44, yes this is redundent, might be changed later
-    elif minutes < 45:
-        rounded_time = time_string[:1] + ".5"
-    #round up to the next hour if 45 or greater
-    else:
-        rounded_time = str(hours + 1) + ".0"
+        #What graphs the user can choose from
+        def graph_options(self):
+            graph_lables_dict = get_data(graph_file_name)
+            i = 0
 
-    return (float(rounded_time))
+            for graph in graph_lables_dict:
+                self.graph_list.insertItem(i, graph)
 
-def cleanup_time(media_dict):
-    ''' Convert all EncodeTime to a valid datetime object and round the runtimes to nearest half hour'''
-    datetime_updated_dict = {}
-    #Run through dict
-    for movie in media_dict:
-        if "EncodeTime" in media_dict[movie]: #Also removes invalid entries
-            #copy the movie data dict
-            movie_data_dict = media_dict[movie]
+        #Get the data from the json file
+        def get_data(self, json_file_name):
+            #Open json file
+            with open(media_file_name) as media_file:
+                #Returns json obj as a dictionary
+                return json.load(media_file)
 
-            #copy encode time string and update it to datetime obj with timezone
-            datetime_encodetime = datetime.strptime(movie_data_dict["EncodeTime"],"%Y:%m:%d %H:%M:%S%z").astimezone()
-            #Call show_length to round the runtimes
-            simple_runtime = show_length(movie_data_dict["MediaOriginalRunTime"])
-            #Find the hour that the show was recorded
-            recorded_hour = datetime_encodetime.hour
+        #Flatten each dictionary entry by replacing the list with the value that was in the list
+        def remove_list (self, media_dict):
+            simple_dict = {}
+            #Run though raw dict
+            for movie in media_dict:
+                #Add new dict entry to simple_dict
+                simple_dict.update({movie:media_dict[movie][0]})
+            return (simple_dict)
 
-            #put the updated time back into the copy of the movie data
-            movie_data_dict.update({"EncodeTime":datetime_encodetime})
-            #put the rounded runtime back into the copy of the movie data
-            movie_data_dict.update({"MediaOriginalRunTime":simple_runtime})
-            #Add hour that show was recorded to the dict
-            movie_data_dict.update({"RecordedHour":recorded_hour})
+        #Round the times in the runtime to the nearest half hour
+        def show_length (self, time_string):
+            minutes = int(time_string[2:4])
+            hours = int(time_string[:1])
+            
+            #round to .5 if under 30
+            if minutes < 30:
+                rounded_time = time_string[:1] + ".5"
+            #round to .5 if 15 to 44, yes this is redundent, might be changed later
+            elif minutes < 45:
+                rounded_time = time_string[:1] + ".5"
+            #round up to the next hour if 45 or greater
+            else:
+                rounded_time = str(hours + 1) + ".0"
 
-            #put the movie and the movie data dict into the new datetime dict
-            datetime_updated_dict.update({movie:movie_data_dict})
+            return (float(rounded_time))
 
-    return datetime_updated_dict
+        #Convert all EncodeTime to a valid datetime object and round the runtimes to nearest half hour
+        def cleanup_time(self, media_dict):
+            datetime_updated_dict = {}
+            #Run through dict
+            for movie in media_dict:
+                if "EncodeTime" in media_dict[movie]: #Also removes invalid entries
+                    #copy the movie data dict
+                    movie_data_dict = media_dict[movie]
 
-def dict_to_dataframe(media_dict):
-    #Put a dict into a dataframe and transpose it
-    return pd.DataFrame(data=media_dict).T
+                    #copy encode time string and update it to datetime obj with timezone
+                    datetime_encodetime = datetime.strptime(movie_data_dict["EncodeTime"],"%Y:%m:%d %H:%M:%S%z").astimezone()
+                    #Call show_length to round the runtimes
+                    simple_runtime = show_length(movie_data_dict["MediaOriginalRunTime"])
+                    #Find the hour that the show was recorded
+                    recorded_hour = datetime_encodetime.hour
 
-def find_all_unique_movies(media_dataframe):
-    #Return all unique movie titles in dataframe
-    return media_dataframe.drop_duplicates(subset=['Title'])
+                    #put the updated time back into the copy of the movie data
+                    movie_data_dict.update({"EncodeTime":datetime_encodetime})
+                    #put the rounded runtime back into the copy of the movie data
+                    movie_data_dict.update({"MediaOriginalRunTime":simple_runtime})
+                    #Add hour that show was recorded to the dict
+                    movie_data_dict.update({"RecordedHour":recorded_hour})
 
-def get_graph_height(height_dataframe, keys_dataframe):
-    height = []
+                    #put the movie and the movie data dict into the new datetime dict
+                    datetime_updated_dict.update({movie:movie_data_dict})
 
-    #Pull the data out of the dataframe
-    for key in keys_dataframe:
-        height.append(height_dataframe[key])
+            return datetime_updated_dict
 
-    return height
+        #Put a dict into a dataframe and transpose it
+        def dict_to_dataframe(self, media_dict):
+            return pd.DataFrame(data=media_dict).T
 
-#Show a graph of a how many shows are watched with a given duration
-def show_duration_graph(show_duration_dataframe):
-    #Get all the keys in the dataframe
-    keys = show_duration_dataframe.keys()
-    #Find the height of the graph data
-    height = get_graph_height(show_duration_dataframe, keys)
+        #Return all unique movie titles in dataframe
+        def find_all_unique_movies(self, media_dataframe):
+            return media_dataframe.drop_duplicates(subset=['Title'])
 
-    #Print Bar chart, bar(x-axis, height)
-    plt.xlabel('Duration in half hours')
-    plt.ylabel('Number of Shows')
-    plt.title('How many shows are watched with a given duration')
-    plt.xticks(range(len(height)), keys)
-    plt.bar(range(len(height)), height)
-    plt.show()
+        #Return the hr=eight data from the graph
+        def get_graph_height(self, height_dataframe, keys_dataframe):
+            height = []
 
-#Show a graph that shows the hour that the show was recorded
-def show_recorded_graph(show_recorded_dataframe):
-    #Get all the keys in the dataframe
-    keys = show_recorded_dataframe.keys()
-    #Find the height of the graph data
-    height = get_graph_height(show_recorded_dataframe, keys)
+            #Pull the data out of the dataframe
+            for key in keys_dataframe:
+                height.append(height_dataframe[key])
 
-    #Print Bar chart, bar(x-axis, height)
-    plt.xlabel('Hour Of The Day')
-    plt.ylabel('Total Shows Recorded At That Hour')
-    plt.title('At What Times Are The Shows Recorded, 24 hour clock')
-    plt.xticks(range(len(height)), keys)
-    plt.bar(range(len(height)), height)
-    plt.show()
+            return height
 
-def main():
+        def build_graph(self, dataframe):
+            pass
+        '''
+        #Show a graph of a how many shows are watched with a given duration
+        def show_duration_graph(show_duration_dataframe):
+            #Get all the keys in the dataframe
+            keys = show_duration_dataframe.keys()
+            #Find the height of the graph data
+            height = get_graph_height(show_duration_dataframe, keys)
+
+            #Print Bar chart, bar(x-axis, height)
+            plt.xlabel('Duration in half hours')
+            plt.ylabel('Number of Shows')
+            plt.title('How many shows are watched with a given duration')
+            plt.xticks(range(len(height)), keys)
+            plt.bar(range(len(height)), height)
+            plt.show()
+
+        #Show a graph that shows the hour that the show was recorded
+        def show_recorded_graph(show_recorded_dataframe):
+            #Get all the keys in the dataframe
+            keys = show_recorded_dataframe.keys()
+            #Find the height of the graph data
+            height = get_graph_height(show_recorded_dataframe, keys)
+
+            #Print Bar chart, bar(x-axis, height)
+            plt.xlabel('Hour Of The Day')
+            plt.ylabel('Total Shows Recorded At That Hour')
+            plt.title('At What Times Are The Shows Recorded, 24 hour clock')
+            plt.xticks(range(len(height)), keys)
+            plt.bar(range(len(height)), height)
+            plt.show()
+        '''
+#def main():
     """
     DVR media analysis -- or see what Mom is watching to find what streaming services would work best for her
     """
@@ -135,10 +167,10 @@ def main():
     #print ("1:45:00 to ", show_length("1:45:00"))
 
     #Pull and clean up the raw file data into a dict
-    clean_moviedict = cleanup_time(remove_list(get_media(media_file_name)))
+    #clean_moviedict = cleanup_time(remove_list(get_media(media_file_name)))
     #print ("Clean movie dictionary:\n", clean_moviedict)
     #Put the movie dict into the pandas dataframe
-    main_movies_dataframe = dict_to_dataframe(clean_moviedict)
+    #main_movies_dataframe = dict_to_dataframe(clean_moviedict)
     #Show the main movie dataframe
     #print ("Main movie dataframe:\n", main_movies_dataframe)
 
@@ -154,9 +186,12 @@ def main():
     #show_duration_graph(main_movies_dataframe.value_counts('MediaOriginalRunTime'))
     
     #Create a graph that shows the hour that the show was recorded, key: EncodeTime, value: datetime object
-    show_recorded_graph(main_movies_dataframe.value_counts('RecordedHour').sort_index())
+    #show_recorded_graph(main_movies_dataframe.value_counts('RecordedHour').sort_index())
     #print(main_movies_dataframe.value_counts('RecordedHour').sort_index())
 
 # This little chunk of code allows this python program to be either used directly or imported into another program
 if __name__ == "__main__":
-    main()    
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
