4a85569c4ccbc8d31f7e1f5aa3954adcb098d4e2
ebenson89 <embox4@gmail.com>
2021-06-25 14:59:45 -0500

    Webpage graphs organized

--- webpage.py
+++ webpage.py
@@ -3,34 +3,55 @@ from bottle import route, run, static_file, template
 import os
 
 graphs_folder = r"./Saved Graphs/"
+media_folder = r"./Saved Graphs/Media/"
+speed_folder = r"./Saved Graphs/Speed/"
 webpagefile = "index.html"
 
 
-def get_graphs():
+def get_graphs(graph_folder, parent_folder):
     """Returns a list of graph plots filenames"""
     graph_name_list = []
     file_names_list = []
 
-    _, _, file_names_list = next(os.walk(graphs_folder))
+    _, _, file_names_list = next(os.walk(graph_folder))
 
     for file_name in file_names_list:
         if file_name[-4:] == ".jpg":
-            graph_name_list.append(file_name)
+            graph_name_list.append("/" + parent_folder + "/" + file_name)
 
     return graph_name_list
 
 
-@route("/<filename>")
-def server_static(filename):
-    """ Get any needed static files -- the graph files """
-    return static_file(filename, root=graphs_folder)
+@route("/speed/<filename>")
+def server_static_speed(filename):
+    """ Get any needed static files -- the speed graph files """
+    return static_file(filename, root='./Saved Graphs/Speed')
+
+
+@route("/media/<filename>")
+def server_static_media(filename):
+    """ Get any needed static files -- the media graph files """
+    return static_file(filename, root='./Saved Graphs/Media')
 
 
 @route("/")
 def webserve():
-    """ Display the graphs being plotted on a web page """
-    graphs = get_graphs()
-    return template(webpagefile, {"graphs": graphs})
+    """ Display links to other pages """
+    return template(webpagefile)
+
+
+@route("/media")
+def media():
+    """ Display the graphs being plotted on the media web page """
+    media = get_graphs(media_folder, "media")
+    return template("media.html", {"media": media})
+
+
+@route("/speed")
+def speed():
+    """ Display the graphs being plotted on the speed web page """
+    speed = get_graphs(speed_folder, "speed")
+    return template("speed.html", {"speed": speed})
 
 
 def main():
